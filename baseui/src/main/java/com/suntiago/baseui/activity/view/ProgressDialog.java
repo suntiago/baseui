4f1a31ecd424d329e4786c26ff951ae57257200f
chuxiao <chuxiao@iyuyun.net>
2019-05-13 15:16:28 +0800

    getDialog.getWindow判空

--- baseui/src/main/java/com/suntiago/baseui/activity/view/ProgressDialog.java
+++ baseui/src/main/java/com/suntiago/baseui/activity/view/ProgressDialog.java
@@ -1,5 +1,6 @@
 package com.suntiago.baseui.activity.view;
 
+import android.app.Dialog;
 import android.app.DialogFragment;
 import android.content.DialogInterface;
 import android.graphics.drawable.AnimationDrawable;
@@ -18,47 +19,62 @@ import com.suntiago.baseui.utils.log.Slog;
 
 public class ProgressDialog extends DialogFragment {
 
-    private static final String TAG = ProgressDialog.class.getSimpleName();
+  private static final String TAG = ProgressDialog.class.getSimpleName();
 
-    public static ProgressDialog newInstance() {
-        ProgressDialog f = new ProgressDialog();
-        return f;
+  public static ProgressDialog newInstance() {
+    ProgressDialog f = new ProgressDialog();
+    return f;
+  }
+
+  @Override
+  public void onActivityCreated(Bundle savedInstanceState) {
+    if (getDialog() == null) {  // Returns mDialog
+      Slog.e(TAG, "ProgressDialog dialog is null");
     }
+    super.onActivityCreated(savedInstanceState);
+  }
 
-    @Override
-    public void onActivityCreated(Bundle savedInstanceState) {
-        if (getDialog() == null) {  // Returns mDialog
-            Slog.e(TAG, "ProgressDialog dialog is null");
-        }
-        super.onActivityCreated(savedInstanceState);
+  @Nullable
+  @Override
+  public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+    Dialog dialog = getDialog();
+    if (dialog == null) {
+      return;
     }
-
-    @Nullable
-    @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        getDialog().getWindow().requestFeature(Window.FEATURE_NO_TITLE);
-        getDialog().getWindow().setBackgroundDrawableResource(android.R.color.transparent);
-
-        View view = inflater.inflate(R.layout.dialog_progress_layout,
-                container, false);
-        ImageView loading = (ImageView) view.findViewById(R.id.dialog_progress_img);
-        AnimationDrawable animationDrawable = (AnimationDrawable) loading.getDrawable();
-        animationDrawable.start();
-        return view;
+    Window window = dialog.getWindow();
+    if (window == null) {
+      return;
     }
+    window.requestFeature(Window.FEATURE_NO_TITLE);
+    window.setBackgroundDrawableResource(android.R.color.transparent);
 
-    @Override
-    public void onResume() {
-        super.onResume();
-        Window window = getDialog().getWindow();
-        int width = getResources().getDimensionPixelSize(R.dimen.dialog_progress_width);
-        int height = getResources().getDimensionPixelSize(R.dimen.dialog_progress_height);
-        window.setLayout(width, height);
-        window.setGravity(Gravity.CENTER);
-    }
+    View view = inflater.inflate(R.layout.dialog_progress_layout,
+            container, false);
+    ImageView loading = (ImageView) view.findViewById(R.id.dialog_progress_img);
+    AnimationDrawable animationDrawable = (AnimationDrawable) loading.getDrawable();
+    animationDrawable.start();
+    return view;
+  }
 
-    @Override
-    public void onDismiss(DialogInterface dialog) {
-        super.onDismiss(dialog);
+  @Override
+  public void onResume() {
+    super.onResume();
+    Dialog dialog = getDialog();
+    if (dialog == null) {
+      return;
     }
+    Window window = dialog.getWindow();
+    if (window == null) {
+      return;
+    }
+    int width = getResources().getDimensionPixelSize(R.dimen.dialog_progress_width);
+    int height = getResources().getDimensionPixelSize(R.dimen.dialog_progress_height);
+    window.setLayout(width, height);
+    window.setGravity(Gravity.CENTER);
+  }
+
+  @Override
+  public void onDismiss(DialogInterface dialog) {
+    super.onDismiss(dialog);
+  }
 }
